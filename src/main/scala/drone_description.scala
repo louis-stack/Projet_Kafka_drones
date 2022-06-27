642866e3dcd4aa4072af410f1c821809d39cc688
vbonneff <victor.bonneff@efrei.net>
2022-06-27 17:05:51 +0200

    ajout case class et producer

---
+++ src/main/scala/drone_description.scala
@@ -0,0 +1,3 @@
+case class drone_description(id: String, latitude: Double, longitude: Double,citizen_name: String, citizen_peacescore: Int, citizen_words: String )
+
+val drone1= drone_description("1", 23, 90, "Didier", 3, "révolution")
