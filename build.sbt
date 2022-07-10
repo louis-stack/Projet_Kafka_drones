e6451a485124787336f15797373bbf7d6b454593
louis <louis.ferrand@efrei.net>
2022-07-11 00:57:58 +0200

    1/4 tu coco

--- build.sbt
+++ build.sbt
@@ -3,6 +3,10 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.8"
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.2.0"
+/*
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.6.0"
+*/
+
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/drone_description.scala
+++
@@ -1,6 +0,0 @@
-case class drone_description(id: String,
-                             latitude: Double,
-                             longitude: Double,
-                             citizen_name: String,
-                             citizen_peacescore: Int,
-                             citizen_words: String )
