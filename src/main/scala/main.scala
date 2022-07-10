231048f1952deff7552f92e4f0c1c2cd3f71d3d4
louis <louis.ferrand@efrei.net>
2022-07-10 22:35:34 +0200

    test

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,21 +7,14 @@ import org.apache.kafka.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 
 
-object main extends App {
-  /*
-  case class Message(sender: String, recipient: String, body: String)
-  val message1 = Message("guillaume@quebec.ca", "jorge@catalonia.es", "Ça va ?")
+object main {
+  def main(args: Array[String]): Unit = {
+    var props:Properties = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, classOf[StringSerializer])
 
-  println("message1.sender " + message1.sender)
-  println("message1.recipient " + message1.recipient)
-  println("message1.body " + message1.body)
-  */
-
-  var props:Properties = new Properties()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, classOf[StringSerializer])
-
-  val producer : KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+    val producer : KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+  }
 
 }
