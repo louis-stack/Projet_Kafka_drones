ac464a878f8b856730cf6c1f1b78b657eec1083c
louis <louis.ferrand@efrei.net>
2022-07-11 11:27:18 +0200

    messages aléatoires

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.8"
 
-libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.2.0"
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.2.0"
 
 
 lazy val root = (project in file("."))
--- src/main/scala/Main1/Producer.scala
+++ src/main/scala/Main1/Producer.scala
@@ -8,20 +8,27 @@ import java.util.Properties
 object Producer extends App {
 
   var props: Properties = new Properties()
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,88.163.185.58:3000")
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,88.163.185.58:9092")
   props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, classOf[StringSerializer])
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
 
   val producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
-  val topic: String = "drone info"
+  val topic: String = "droneInfo"
 
   sendInfo(producer)
 
   def sendInfo(kafkaproducer: KafkaProducer[String, String]): Unit = {
     val drone = DroneRapport.createDrone()
     val droneString = DroneRapport.droneToJson(drone)
+    println("drone.id : " + drone.id + "droneString : " + droneString)
     val record = new ProducerRecord[String, String](topic, drone.id, droneString)
-    producer.send(record)
-    sendInfo(kafkaproducer)
+    val data_envoyees = producer.send(record)
+    printf(s"sent record(key=%s value=%s) " +
+      "meta(partition=%d, offset=%d)\n",
+      record.key(), record.value(),
+      data_envoyees.get().partition(),
+      data_envoyees.get().offset())
+
   }
+  producer.close()
 }
